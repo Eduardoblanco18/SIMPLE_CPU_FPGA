c847a468d9d89ab595c4a79f0352d19049d2c006
EDUARDO DOMINGUES BLANCO <eduardo.db1@puccampinas.edu.br>
2026-05-20 12:57:30 -0300

    Changing the UC architecture

--- src/UC.vhd
+++ src/UC.vhd
@@ -2,29 +2,28 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity UC is
-	generic (n: integer :=8);
 	port (
 			clock_UC, reset_UC : in  std_logic;
 
-			SW: in std_logic_vector (15 downto 0);
+			instruction: in std_logic_vector (7 downto 0);
 			
 			bus_select: out std_logic_vector (2 downto 0);
 			
 			R0in, R1in, Ain : out std_logic;
 			
-			ALU_op_code: out std_logic_vector (2 downto 0)
+			ALU_op_code: out std_logic_vector (1 downto 0)
 			);
 	end entity UC;
 	
 architecture bhv of UC is
 	
-	type t_State is (Fetch, Decode, Operand_fetch, Execution);
+	type t_State is (Fetch, Decode, Execution1, Execution2, Execution3);
 	
 	signal current_state : t_State;
 
 	signal OP_CODE: std_logic_vector(3 downto 0);
 	signal reg1, reg2: std_logic_vector (1 downto 0);
-	signal Operand: std_logic_vector(n-1 downto 0);
+	signal exec: std_logic_vector (1 downto 0);
 	
 	begin
 		
@@ -43,13 +42,23 @@ architecture bhv of UC is
 						
 						
 					
-					else 
+					else
+						
+						bus_select <= (others => '0');
+						R0in <= '0';
+						R1in <= '0';
+						Ain <= '0';
+						ALU_op_code <= (others => '0');
 						
 						case current_state is
 							
 							when Fetch => 
 								
-								OP_CODE <= SW (15 downto 12);
+								OP_CODE <= instruction (7 downto 4);
+								
+								reg1 <= instruction (3 downto 2);
+								
+								reg2 <= instruction (1 downto 0);
 								
 								current_state <= Decode;
 							
@@ -57,18 +66,120 @@ architecture bhv of UC is
 
 								case OP_CODE is
 									when "0000" =>
+										exec <= "00";
+									when "0001" => 
+										exec <= "01";
+									when "0010" =>
+										exec <= "10";
+									when "0011" =>
+										exec <= "11";
+									when others =>
+										exec <= "00";
 								end case;
 							
-								current_state <= Operand_fetch;		
+								current_state <= execution1;		
 							
-							when Operand_fetch =>
+							when Execution1 =>
+								
+								case exec is
+									when "01" =>
+										bus_select <= "101";
+										
+										case reg1 is
+											when "00" =>
+												R0in <= '1';
+											when "01" =>
+												R1in <= '1';
+											when others =>
+												null;
+										end case;
+										
+										current_state <= Fetch;
+									
+									when "10" =>
+										
+										case reg2 is
+											when "00" =>
+												bus_select <= "001";
+											when "01" =>
+												bus_select <= "010";
+											when others =>
+												null;
+										end case;
+											
+										Ain <= '1';
+										
+										current_state <= Execution2;
+									
+									when "11" =>
+										
+										case reg2 is
+											when "00" =>
+												bus_select <= "001";
+											when "01" =>
+												bus_select <= "010";
+											when others =>
+												null;
+										end case;
+										
+										Ain <= '1';
+										
+										current_state <= Execution2;
+										
+									when others =>
+										current_state <= Fetch;
+								end case;
 							
-								current_state <= Execution;
+							when Execution2 =>
+								
+								case exec is
+									when "10" =>
+										
+										case reg1 is
+											when "00" =>
+												bus_select <= "001";
+											when "01" =>
+												bus_select <= "010";
+											when others =>
+												null;
+										end case;
+										
+										ALU_op_code <= "01";
+										
+										current_state <= Execution3;
+									
+									when "11" =>
+										
+										case reg1 is
+											when "00" =>
+												bus_select <= "001";
+											when "01" =>
+												bus_select <= "010";
+											when others =>
+												null;
+										end case;
+										
+										ALU_op_code <= "10";
+										
+										current_state <= Execution3;
+									
+									when others =>
+										current_state <= Fetch;
+								end case;
 							
-							when Execution =>
-						
+							when Execution3 =>
+								bus_select <= "100";
+								
+								case reg1 is
+											when "00" =>
+												R0in <= '1';
+											when "01" =>
+												R1in <= '1';
+											when others =>
+												null;
+										end case;
+								
 								current_state <= Fetch;
-						
 						end case;
 					end if;
 				end if;
